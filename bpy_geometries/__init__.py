1ce37e67e9bd4271c5aba656bdbbf2478f53ae57
Harry Ballington <hballington12@gmail.com>
2025-11-06 15:33:49 +0100

    update __init__

--- bpy_geometries/__init__.py
+++ bpy_geometries/__init__.py
@@ -1,18 +1,22 @@
+from .aggregate import Aggregate
+from .bevel import Bevel
+from .droxtal import Droxtal
 from .geometry import Geometry
-from .hexagonal_column import HexagonalColumn
-from .indented_column import IndentedColumn
 from .hexagonal_bullet import HexagonalBullet
 from .hexagonal_bullet_rosette import HexagonalBulletRosette
-from .droxtal import Droxtal
+from .hexagonal_column import HexagonalColumn
+from .inclusions import Inclusions
+from .indented_column import IndentedColumn
 from .roughened import Roughened
-from .aggregate import Aggregate
 
 __all__ = [
+    "Bevel",
     "Geometry",
     "HexagonalColumn",
     "IndentedColumn",
     "HexagonalBullet",
     "HexagonalBulletRosette",
+    "Inclusions",
     "Droxtal",
     "Roughened",
     "Aggregate",
